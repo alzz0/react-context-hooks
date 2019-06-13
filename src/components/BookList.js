30cc81ec2ec1602cac5719c248ff1f0e6fd1331c
Ali Mansour <alimansour@Alis-MacBook-Pro.local>
2019-06-13 13:27:47 -0300

    initial commit

---
+++ src/components/BookList.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
+class BookList extends Component {
+
+  render() {
+    
+    return (
+        
+        <ThemeContext.Consumer>{(context)=>{
+                const { isLightTheme, light, dark } = context;
+    const theme = isLightTheme ? light : dark;
+                return(
+                        <div className="book-list" style={{background:theme.bg,color:theme.syntax}}>
+        <ul>
+          <li style={{background:theme.ui}}>Book 1</li>
+          <li style={{background:theme.ui}}>Book 2</li>
+          <li style={{background:theme.ui}}>Book 3</li>
+          <li style={{background:theme.ui}}>Book 4</li>
+          <li style={{background:theme.ui}}>Book 5</li>
+        </ul>
+      </div>
+                )
+                
+            }}
+        </ThemeContext.Consumer>
+  
+    );
+  }
+}
+export default BookList;
